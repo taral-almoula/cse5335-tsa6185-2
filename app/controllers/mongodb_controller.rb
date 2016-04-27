aab56889ec9994a27bf0d002a3cf9d84a10d0fca
taral-almoula <taral.almoula@gmail.com>
2016-04-26 20:16:09 -0500

    Commit 6

--- app/controllers/mongodb_controller.rb
+++ app/controllers/mongodb_controller.rb
@@ -9,7 +9,7 @@ i = 0
 count = 0
 
 database = Mongo::Client.new([ 'ds047652.mlab.com:47652' ], :database => 'heroku_bwj16nq9', :user => 'taral', :password => 'taral')
-puts '-------------------------------------------------------------------------------------------------------------------------'
+
 while i < 1  do
 	#DATA SOURCE: http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json 
     data = JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json").read)
